696488a73b7c65b82d056387bb970da969000347
Daniel Dias <danieldias15@gmail.com>
2020-04-28 12:54:36 -0400

    Updated Model to have parameters match and changed the Loss function

--- src/model/main_model.py
+++ src/model/main_model.py
@@ -6,7 +6,7 @@ import torch.nn as nn
     https://arxiv.org/ftp/arxiv/papers/1804/1804.07300.pdf
 '''
 class MusicGeneration(nn.Module):
-    def __init__(self, time_sequence_len, batch_size, time_hidden_size):
+    def __init__(self, time_sequence_len, batch_size, time_hidden_size, data_type=torch.DoubleTensor):
         super(MusicGeneration, self).__init__()
 
         self.num_layers = 1
@@ -15,11 +15,12 @@ class MusicGeneration(nn.Module):
         self.time_sequence_len = time_sequence_len
         self.batch_size = batch_size
         self.time_hidden_size = time_hidden_size
+        self.data_type = data_type
         self.init_hidden()
 
         # self.lstm_time0 = nn.LSTM(input_size=12 * 2, hidden_size=time_hidden_size, batch_first=True)
         self.lstm_time0 = nn.LSTM(input_size=78 * 2, hidden_size=time_hidden_size, batch_first=True)
-        self.lstm_note0 = nn.LSTM(input_size=time_sequence_len * time_hidden_size, hidden_size=78 * 2, batch_first=True)
+        self.lstm_note0 = nn.LSTM(input_size=time_sequence_len, hidden_size=78 * 2, batch_first=True)
 
         self.dropout = nn.Dropout(p=0.2)
 
@@ -27,23 +28,26 @@ class MusicGeneration(nn.Module):
         # h_0.shape = (num_layers * num_directions, batch, hidden_size)
         # c_0.shape = (num_layers * num_directions, batch, hidden_size)
         self.hidden_time0 = (
-            torch.ones(self.num_layers * self.num_directions, self.batch_size, self.time_hidden_size, dtype=torch.float64),
-            torch.ones(self.num_layers * self.num_directions, self.batch_size, self.time_hidden_size, dtype=torch.float64),
+            torch.ones(self.num_layers * self.num_directions, self.batch_size, self.time_hidden_size).type(self.data_type),
+            torch.ones(self.num_layers * self.num_directions, self.batch_size, self.time_hidden_size).type(self.data_type),
         )
         self.hidden_note0 = (
-            torch.ones(self.num_layers * self.num_directions, self.batch_size, self.time_hidden_size * self.time_sequence_len, dtype=torch.float64),
-            torch.ones(self.num_layers * self.num_directions, self.batch_size, self.time_hidden_size * self.time_sequence_len, dtype=torch.float64),
+            torch.ones(self.num_layers * self.num_directions, self.batch_size, 78 * 2).type(self.data_type),
+            torch.ones(self.num_layers * self.num_directions, self.batch_size, 78 * 2).type(self.data_type),
         )
 
     def to(self, *args, **kwargs):
-        self = super().to(*args, **kwargs) 
+        self = super().to(*args, **kwargs)
         self.hidden_time0 = tuple(hx.to(*args, **kwargs) for hx in self.hidden_time0)
         self.hidden_note0 = tuple(hx.to(*args, **kwargs) for hx in self.hidden_note0)
         return self
 
     def forward(self, x):
         time_outs, hidden_time_n = self.lstm_time0(x, self.hidden_time0)
-        note_outs, hidden_note_n = self.lstm_note0(time_outs, self.hidden_note0)
+        # time_outs.shape = torch.Size([10, 256, 36])
 
-        y_pred = torch.where(note_outs > 0.5, 1.0, 0.0)
+        x_note = time_outs.permute(0, 2, 1)
+        note_outs, hidden_note_n = self.lstm_note0(x_note, self.hidden_note0)
+
+        y_pred = torch.where(note_outs > 0.5, torch.ones(note_outs.shape), torch.zeros(note_outs.shape))
         return y_pred
